a3986254f634a86e8f787d9d58107fdd61093a46
Javlon1221 <avazbekzalilov835@gmail.com>
2025-08-02 14:54:56 +0500

    fiix final exam

--- src/pages/saved/index.tsx
+++ src/pages/saved/index.tsx
@@ -17,6 +17,9 @@ const Saved = () => {
 
   return (
     <>
+    <div>
+
+    </div>
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold">Saved Students</h2>
         <button
@@ -25,8 +28,8 @@ const Saved = () => {
         >
           Remove All
         </button>
-      <StudentView data={savedStudents} />
       </div>
+      <StudentView data={savedStudents} />
     </>
   );
 };
